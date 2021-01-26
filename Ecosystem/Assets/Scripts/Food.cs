203414ca9cf6c8b049350e52afd48ae41c1b5500
Viktor Fredholm <vikfre@student.chalmers.se>
2021-01-26 09:52:56 +0100

    Herbivore and Food blob

---
+++ Ecosystem/Assets/Scripts/Food.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    [SerializeField] private int saturation;
+
+    public int Saturation => saturation;
+
+    public void Consume()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Ecosystem/Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        var newPos = new Vector3(Random.Range(-1,2), 0, Random.Range(-1,2)) * Time.deltaTime;
+        
+        controller.Move(newPos);
+    }
+
+    private Transform GetClosestFood()
+    {
+        
+
+        return null;
+    }
+
+}
+
+   
