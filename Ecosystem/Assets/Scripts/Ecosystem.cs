cc47e40d2ccfd5a8148e0d4bbb8549502ed1b3b8
Theo <theo.le.wiik@gmail.com>
2021-02-01 11:07:59 +0100

    Add ability to log the amount of animals for each day

--- Ecosystem/Assets/Scripts/Ecosystem.cs
+++ Ecosystem/Assets/Scripts/Ecosystem.cs
@@ -1,10 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using Logger;
+using UnityEngine;
 
-public class Ecosystem : MonoBehaviour
+public sealed class Ecosystem : MonoBehaviour
 {
+  private DataLogger _logger;
+  
   private void Start()
   {
-    var logger = DataLogger.Instance;
-    logger.Do();
+    _logger = DataLogger.Instance;
+    _logger.InitializeLogging();
+    _logger.Snapshot(0, GetAllAnimals());
+  }
+
+  private IList<Animal> GetAllAnimals()
+  {
+    // TODO: This should DEFINITELY be changed so that the ecosystem has a list of animals which it adds to when instancing the animals.
+    var animals = FindObjectsOfType<Animal>();
+    return animals;
   }
 }
---
+++ Ecosystem/Assets/Scripts/Logger/DataLogger.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Logger
+{
+  public sealed class DataLogger
+  {
+    //private IList<LoggableColumn> _loggableColumns;
+    private const string Delimiter = ",";
+    private const string Path = "Assets/test.txt";
+
+    static DataLogger()
+    {
+      // Explicit static constructor to tell C# compiler not to mark type as beforefieldinit
+    }
+
+    private DataLogger()
+    {
+      // _loggableColumns = new List<LoggableColumn>();
+    }
+
+    public static DataLogger Instance { get; } = new DataLogger();
+
+    private void Clear()
+    {
+      File.WriteAllText(Path, string.Empty);
+    }
+
+    /// <summary>
+    ///   Clears the log file and appends the header.
+    /// </summary>
+    public void InitializeLogging()
+    {
+      Clear();
+      AppendHeader();
+    }
+
+    private void AppendHeader()
+    {
+      var writer = File.AppendText(Path);
+      writer.WriteLine("day" + Delimiter + "amount");
+      writer.Close();
+    }
+
+    public void Snapshot(int day, IList<Animal> animals)
+    {
+      var amount = animals.Count;
+
+      var writer = File.AppendText(Path);
+      writer.WriteLine(day + Delimiter + amount);
+      writer.Close();
+    }
+
+    private string CreateRow()
+    {
+      var sb = new StringBuilder();
+      sb.Append("first column");
+      sb.Append(Delimiter);
+      sb.Append("second column");
+
+      return sb.ToString();
+    }
+  }
+}
