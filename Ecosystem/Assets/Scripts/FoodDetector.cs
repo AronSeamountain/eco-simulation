49099d288ec023135bb34fc8bf619dc6499e6b39
Viktor Fredholm <vikfre@student.chalmers.se>
2021-02-02 13:47:11 +0100

    Blob succesfully detects watersource

--- Ecosystem/Assets/Scripts/FoodDetector.cs
+++ Ecosystem/Assets/Scripts/FoodDetector.cs
@@ -10,11 +10,20 @@ public sealed class FoodDetector : MonoBehaviour
   /// </summary>
   /// <param name="food">The found that was just found.</param>
   public delegate void FoodFound(Food food);
+  
+  /// <summary>
+  ///   Gets invoked when water is found.
+  /// </summary>
+  /// <param name="water">The water that was just found.</param>
+  public delegate void WaterFound(Water water);
 
   public FoodFound FoodFoundListeners;
+  public WaterFound WaterFoundListeners;
 
   private void OnTriggerEnter(Collider other)
   {
     if (other.GetComponent<Food>() is Food food) FoodFoundListeners?.Invoke(food);
+
+    if (other.GetComponent<Water>() is Water water) WaterFoundListeners?.Invoke(water);
   }
 }
