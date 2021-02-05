7c60e28ac6cc1ab06e1822380c68dcedb3f124e8
Yvonne Hansson <gushansyv@student.gu.se>
2021-02-05 15:30:47 +0100

    Added cards class and StatsCardUI (don't know if needed yet))

--- Ecosystem/Assets/Scripts/DisplayCharacterStats.cs
+++ Ecosystem/Assets/Scripts/DisplayCharacterStats.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class DisplayCharacterStats : MonoBehaviour
 {
   [SerializeField] private Camera mainCamera;
+  [SerializeField] private Animal animal;
+  private StatsCardUi _card;
   private CameraControls _controls;
   private Transform _target;
 
@@ -12,14 +14,12 @@ public class DisplayCharacterStats : MonoBehaviour
     _controls = new CameraControls();
     _controls.CameraMovement.Selecting.performed += ClickedChar;
   }
-  
 
   // Update is called once per frame
   private void Update()
   {
-    
   }
-  
+
   private void OnEnable()
   {
     _controls.Enable();
@@ -37,18 +37,17 @@ public class DisplayCharacterStats : MonoBehaviour
 
     if (Physics.Raycast(ray, out hitTarget))
       _target = hitTarget.transform;
+    //if (_target != animal.transform) return; // does not work
     DisplayStats(_target);
-
   }
-  
+
   private Vector2 GetMousePos()
   {
     return Mouse.current.position.ReadValue();
   }
 
   /// <summary>
-  /// Don't know what exactly what it should return yet, so I made it void. Also, method should perhaps take in an Animal, not a Transform?
-  /// When clicking on a object, it returns "clicked".
+  ///  Display stats of clicked animal.
   /// </summary>
   /// <param name="target"></param>
   private void DisplayStats(Transform target)
---
+++ Ecosystem/Assets/Scripts/StatsCardUi.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class StatsCardUi : MonoBehaviour
+{
+  /// <summary>
+  /// TODO: Can all of this be moved into DisplayCharacterStats.cs?
+  /// </summary>
+  [SerializeField] private GameObject cardPrefab;
+
+  /// <summary>
+  ///   Putting the card at the bottom at the screen to start with, will adjust later.
+  /// </summary>
+  private Vector2 _bottomCenterScreenSpace;
+  private Vector3 _bottomCenterWorldSpace;
+
+  private void Start()
+  {
+    this._bottomCenterScreenSpace = new Vector2(Screen.width / 2,0);
+    this._bottomCenterWorldSpace = Camera.main.ScreenToWorldPoint(new Vector3(_bottomCenterScreenSpace.x, _bottomCenterWorldSpace.y, Camera.main.nearClipPlane));
+  }
+  private void Update()
+  {
+    //OnCardDrawn();
+  }
+  
+  private void OnCardDrawn(Card card)
+  {
+    var cardPosition = _bottomCenterWorldSpace;
+    GameObject newCard = GameObject.Instantiate(this.cardPrefab, this._bottomCenterWorldSpace, Quaternion.identity);
+  }
+  
+}
