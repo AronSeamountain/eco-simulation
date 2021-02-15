92ffa1ae3675f380bd1b90075858d8950c7d1664
Erik Söderpalm <soderpalmeab@gmail.com>
2021-02-15 12:57:06 +0100

    Added text to the sliders
    Right now the text is set to 100 for max, whichever bar it is. Will try to change to MaxSaturation etc.

---
+++ Ecosystem/Assets/Scripts/BarText.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarText : MonoBehaviour
+{
+  [SerializeField] private string valueText = "{0}";
+
+  private Text _text;
+
+  // Start is called before the first frame update
+  private void Start()
+  {
+    _text = GetComponent<Text>();
+
+    GetComponentInParent<Slider>().onValueChanged.AddListener(ValueChanged);
+  }
+
+  private void ValueChanged(float value)
+  {
+    _text.text = string.Format(valueText + "/" + GetComponentInParent<Slider>().maxValue*100, value*100);
+  }
+}
