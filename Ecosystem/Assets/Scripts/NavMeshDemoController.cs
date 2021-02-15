6a51e85c04cb9d53d9257b8fa8a48a0fbdd9e97d
Theo <theo.le.wiik@gmail.com>
2021-02-15 10:52:20 +0100

    Add super basic navmesh demo

---
+++ Ecosystem/Assets/Scripts/NavMeshDemoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public sealed class NavMeshDemoController : MonoBehaviour
+{
+  [SerializeField] private NavMeshAgent agent;
+  [SerializeField] private Camera camera;
+
+  private void Update()
+  {
+    var mousePos = Mouse.current.position.ReadValue();
+
+    var ray = camera.ScreenPointToRay(mousePos);
+
+    if (Physics.Raycast(ray, out var hit))
+    {
+      agent.SetDestination(hit.point);
+    }
+  }
+}
