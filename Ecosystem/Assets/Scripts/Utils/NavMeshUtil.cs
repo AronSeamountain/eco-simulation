78f9dfc212dd42fb6b50796254d883b1f9a93f4f
Theo <theo.le.wiik@gmail.com>
2021-02-17 23:35:40 +0100

    Create a util class for navmesh

---
+++ Ecosystem/Assets/Scripts/Utils/NavMeshUtil.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Utils
+{
+  public static class NavMeshUtil
+  {
+    /// <summary>
+    ///   Gets a random point of the nav mesh. Credits to Puck for solution
+    ///   (https://answers.unity.com/questions/857827/pick-random-point-on-navmesh.html).
+    /// </summary>
+    /// <returns>A random point of the nav mesh.</returns>
+    public static Vector3 GetRandomLocation()
+    {
+      var navMeshData = NavMesh.CalculateTriangulation();
+
+      // Pick the first indice of a random triangle in the nav mesh
+      var t = Random.Range(0, navMeshData.indices.Length - 3);
+
+      // Select a random point on it
+      var point = Vector3.Lerp(navMeshData.vertices[navMeshData.indices[t]],
+        navMeshData.vertices[navMeshData.indices[t + 1]], Random.value);
+      Vector3.Lerp(point, navMeshData.vertices[navMeshData.indices[t + 2]], Random.value);
+
+      return point;
+    }
+  }
+}
