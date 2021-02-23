6369a4d5037fbf38679058437ad67d0d12d2e3d1
Yvonne Hansson <gushansyv@student.gu.se>
2021-02-23 10:39:40 +0100

    Added a ListeningArea to base animal

---
+++ Ecosystem/Assets/Scripts/Animal/HearingDetector.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Animal
+{
+  /// <summary>
+  ///   Triggers on sounds from other animals
+  /// </summary>
+  public sealed class HearingDetector : MonoBehaviour
+  {
+    /// <summary>
+    /// Is invoked when another animal is heard.
+    /// </summary>
+    /// <param name="animal"></param>
+    public delegate void AnimalHeard(AbstractAnimal animal);
+
+    [SerializeField] private Transform hearingTransform;
+    private int _radius;
+    public AnimalHeard AnimalHeardListeners;
+    
+    private int Radius
+    {
+      get => _radius;
+      set => _radius = Mathf.Clamp(value, 0, int.MaxValue);
+    }
+
+    private void Start()
+    {
+      _radius = 8;
+    }
+    
+    
+
+  } 
+  
+}
