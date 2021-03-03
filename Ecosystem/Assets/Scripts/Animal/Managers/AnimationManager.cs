7a9623f558bfb58f1e28fc0e21f199c11dca98d2
Erik Söderpalm <soderpalmeab@gmail.com>
2021-03-03 14:23:35 +0100

    Added a float multiplier to animations

--- Ecosystem/Assets/Scripts/Animal/Managers/AnimationManager.cs
+++ Ecosystem/Assets/Scripts/Animal/Managers/AnimationManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Animal.AnimalStates;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Animal.Managers
 {
@@ -14,6 +15,8 @@ namespace Animal.Managers
     private static readonly int State = Animator.StringToHash("State");
     [SerializeField] private Animator animator;
     [SerializeField] private AudioSource audioSource;
+    [SerializeField] private NavMeshAgent agent;
+    private static readonly int AnimationSpeed = Animator.StringToHash("AnimationSpeed");
 
     public void ReceiveState(AnimalState state)
     {
@@ -21,23 +24,33 @@ namespace Animal.Managers
       {
         case AnimalState.Birth:
           animator.SetInteger(State, Birth);
+          animator.SetFloat(AnimationSpeed, 1);
           break;
         case AnimalState.Dead:
           animator.SetInteger(State, Dead);
+          animator.SetFloat(AnimationSpeed, 1);
           break;
         case AnimalState.Wander:
           animator.SetInteger(State, Wander);
+          agent.speed = 1;
+          animator.SetFloat(AnimationSpeed, 1);
           break;
         case AnimalState.PursueFood:
         case AnimalState.PursueMate:
         case AnimalState.PursueWater:
         case AnimalState.Hunt:
           animator.SetInteger(State, Pursue);
+          agent.speed = 4;
+          animator.SetFloat(AnimationSpeed, 1.5f);
           break;
         case AnimalState.Eat:
         case AnimalState.Drink:
+          animator.SetInteger(State, Idle);
+          animator.SetFloat(AnimationSpeed, 2.5f);
+          break;
         case AnimalState.Idle:  
           animator.SetInteger(State, Idle);
+          animator.SetFloat(AnimationSpeed, 1);
           break;
         default:
           throw new ArgumentOutOfRangeException(nameof(state), state, null);
