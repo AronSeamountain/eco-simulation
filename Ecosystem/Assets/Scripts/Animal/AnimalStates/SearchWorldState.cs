a3157c52278725d8b217d325f6f6db7ca9623fe8
Yvonne Hansson <gushansyv@student.gu.se>
2021-03-12 16:05:32 +0100

    Added SearchWorld state, not implemented any logic yet

---
+++ Ecosystem/Assets/Scripts/Animal/AnimalStates/SearchWorldState.cs
@@ -0,0 +1,35 @@
+﻿using Core;
+
+namespace Animal.AnimalStates
+{
+  public sealed class SearchWorldState : IState<AnimalState>
+  {
+    private readonly AbstractAnimal _animal;
+
+    public SearchWorldState(AbstractAnimal animal)
+    {
+      _animal = animal;
+    }
+
+    public AnimalState GetStateEnum()
+    {
+      return AnimalState.SearchWorld;
+    }
+
+    public void Enter()
+    {
+    }
+
+    public AnimalState Execute()
+    {
+      //if animal has eaten, but can't find water, it should go into this state
+      // similarly, if it has drunk enough water but cannot find food, it should go into this state.
+      
+      return AnimalState.SearchWorld;
+    }
+
+    public void Exit()
+    {
+    }
+  }
+}
