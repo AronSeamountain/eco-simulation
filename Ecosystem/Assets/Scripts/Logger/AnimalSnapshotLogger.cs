ad450c792a9839aac0ca4ce2091ce8e3c454c678
Theo <theo.le.wiik@gmail.com>
2021-03-12 16:58:36 +0100

    Write animals to json file

--- Ecosystem/Assets/Scripts/Logger/AnimalSnapshotLogger.cs
+++ Ecosystem/Assets/Scripts/Logger/AnimalSnapshotLogger.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Animal;
 using UnityEngine;
@@ -8,39 +9,29 @@ namespace Logger
   public sealed class AnimalSnapshotLogger
   {
     private const string Path = "Assets/Logs/animal_log.json";
-    private IList<AnimalSnapshot> _snapshots;
+    private readonly IList<string> _snapshots;
 
     static AnimalSnapshotLogger()
     {
-      // Explicit static constructor to tell C# compiler not to mark type as beforefieldinit
     }
 
     private AnimalSnapshotLogger()
     {
-      _snapshots = new List<AnimalSnapshot>();
-    }
-
-    /// <summary>
-    ///   A snapshot of an animal. Does only contain relevant fields.
-    /// </summary>
-    private struct AnimalSnapshot
-    {
-      public AnimalSpecies species;
-
-      public AnimalSnapshot(AbstractAnimal animal)
-      {
-        species = animal.Species;
-      }
+      _snapshots = new List<string>();
     }
 
     public static AnimalSnapshotLogger Instance { get; } = new AnimalSnapshotLogger();
 
     public void Persist()
     {
-      var jsonString = JsonUtility.ToJson(_snapshots);
+      File.WriteAllText(Path, string.Empty);
 
       var writer = File.AppendText(Path);
-      writer.WriteLine(jsonString);
+
+      writer.WriteLine("[");
+      foreach (var snapshot in _snapshots) writer.WriteLine(snapshot + ",");
+      writer.WriteLine("]");
+
       writer.Close();
     }
 
@@ -49,7 +40,29 @@ namespace Logger
       foreach (var animal in animals)
       {
         var snapshot = new AnimalSnapshot(animal);
-        _snapshots.Add(snapshot);
+        var snapshotJson = JsonUtility.ToJson(snapshot);
+        Debug.Log(snapshotJson);
+        _snapshots.Add(snapshotJson);
+      }
+    }
+
+    /// <summary>
+    ///   A snapshot of an animal. Does only contain relevant fields.
+    /// </summary>
+    [Serializable]
+    private struct AnimalSnapshot
+    {
+      public string species;
+      public float speed;
+      public float size;
+      public int age;
+
+      public AnimalSnapshot(AbstractAnimal animal)
+      {
+        species = animal.Species.ToString();
+        speed = animal.SpeedModifier;
+        size = animal.SizeModifier;
+        age = animal.AgeInDays;
       }
     }
   }
