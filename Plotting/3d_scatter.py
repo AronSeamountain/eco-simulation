89d8e077ed3361552fb9d6ca2f9d681a3e9050fd
Theo <theo.le.wiik@gmail.com>
2021-03-17 22:55:14 +0100

    Add 3d scatter

---
+++ Plotting/3d_scatter.py
@@ -0,0 +1,39 @@
+import glob
+import json
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+def extract(key, data):
+    return [i[key] for i in data]
+
+
+file_name = 'animal_log.json'
+text_files = glob.glob("./**/" + file_name, recursive=True)
+
+if not text_files:
+    raise Exception('Found no ' + file_name + ' file.')
+
+full_path = text_files[0]
+
+f = open(full_path)
+data = json.load(f)
+
+day = extract('day', data)
+size = extract('size', data)
+speed = extract('speed', data)
+species = extract('species', data)
+
+df = px.data.iris()
+
+fig = px.scatter_3d(
+    df,
+    x=day,
+    y=speed,
+    z=size,
+    color=species,
+    labels={'x': 'day', 'y': 'speed', 'z': 'size', 'color': 'species'}
+)
+
+fig.show()
--- Plotting/average.py
+++ Plotting/average.py
@@ -4,16 +4,21 @@ import pandas as pd
 import plotly.express as px
 
 file_name = 'log.csv'
-text_files = glob.glob("./**/" + file_name, recursive = True)
+text_files = glob.glob("./**/" + file_name, recursive=True)
 
 if not text_files:
-  raise Exception('Found no ' + file_name + ' file.')
+    raise Exception('Found no ' + file_name + ' file.')
 
 full_path = text_files[0]
 
 # Plot
 df = pd.read_csv(full_path)
-#fig = px.line(df, x = 'day', y = 'value', title='My Cool Graph')
-fig = px.scatter_3d(df, x='day', y='speed', z='size', color='amount')
+fig = px.scatter_3d(
+    df,
+    x='day',
+    y='speed',
+    z='size',
+    color='amount'
+)
 
 fig.show()
