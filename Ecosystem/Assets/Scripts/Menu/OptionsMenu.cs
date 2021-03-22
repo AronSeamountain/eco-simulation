1c96cd4cf417267ac8bb2fc595dcb3b658523ce2
Emil Wingårdh <wiemil@student.chalmers.se>
2021-03-22 09:10:01 +0100

    Removed DebugLog

--- Ecosystem/Assets/Scripts/Menu/OptionsMenu.cs
+++ Ecosystem/Assets/Scripts/Menu/OptionsMenu.cs
@@ -12,25 +12,21 @@ namespace Menu
     public void SetRabbits(string number)
     {
       EntityManager.InitialRabbits = int.Parse(number);
-      Debug.Log("Rabbits: " + EntityManager.InitialRabbits);
     }
 
     public void SetWolves(string number)
     {
       EntityManager.InitialWolves = int.Parse(number);
-      Debug.Log("Wolves: " + EntityManager.InitialWolves);
     }
 
     public void SetPlants(string number)
     {
       EntityManager.InitialPlants = int.Parse(number);
-      Debug.Log("Plants: " + EntityManager.InitialPlants);
     }
 
     public void SetHoursInRealSeconds(string number)
     {
       EntityManager.HoursInRealSeconds = float.Parse(number);
-      Debug.Log("Hours in real seconds: " + EntityManager.HoursInRealSeconds);
     }
 
     public void SetWorld(int number)
@@ -43,8 +39,6 @@ namespace Menu
       {
         World = "World";
       }
-
-      Debug.Log("World: " + World);
     }
   }
 
