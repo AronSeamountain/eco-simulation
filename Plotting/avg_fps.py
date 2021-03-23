0bb6cd601283133340ad895333545ccd6afa2c90
Theo <theo.le.wiik@gmail.com>
2021-03-23 11:12:39 +0100

    Plot amount of animals in fps graph

--- Plotting/avg_fps.py
+++ Plotting/avg_fps.py
@@ -1,25 +1,39 @@
 import pandas as pd
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 from util.file_finder import get_full_path
 
 
 def plot():
-    full_path = get_full_path('fps.csv')
-    df = pd.read_csv(full_path)
-    fig = go.Figure()
+    overview_full_path = get_full_path('overview.csv')
+    df_overview = pd.read_csv(overview_full_path)
+
+    fps_full_path = get_full_path('fps.csv')
+    df_fps = pd.read_csv(fps_full_path)
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
 
     fig.add_trace(
         go.Scatter(
-            x=df['day'], y=df['average'], name='average fps', line=dict(color='rebeccapurple', width=4)
-        )
+            x=df_fps['day'], y=df_fps['average'], name='average fps', line=dict(color='royalblue', width=4)
+        ),
+        secondary_y=False
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=df_overview['day'], y=df_overview['amount'], name='# animals', line=dict(color='darkgrey', width=4)
+        ),
+        secondary_y=True
     )
 
     fig.update_layout(
         title='Average FPS',
         xaxis_title='day',
-        yaxis_title='average fps'
     )
+
+    fig.update_yaxes(title_text='average fps', secondary_y=False)
+    fig.update_yaxes(title_text='# animals', secondary_y=True)
 
     fig.show()
 
