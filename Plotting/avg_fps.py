fcf380437bc706655b73aa542cc34d3a26f1f5a8
Theo <theo.le.wiik@gmail.com>
2021-03-23 11:56:22 +0100

    Show fps and # animals baseline on fps graph

--- Plotting/avg_fps.py
+++ Plotting/avg_fps.py
@@ -6,23 +6,60 @@ from util.file_finder import get_full_path
 
 
 def plot():
+    fps_full_path = get_full_path('fps.csv')
+    df_fps = pd.read_csv(fps_full_path)
+
+    fps_baseline_full_path = get_full_path('fps_baseline.csv')
+    df_fps_baseline = pd.read_csv(fps_baseline_full_path)
+
     overview_full_path = get_full_path('overview.csv')
     df_overview = pd.read_csv(overview_full_path)
 
-    fps_full_path = get_full_path('fps.csv')
-    df_fps = pd.read_csv(fps_full_path)
+    overview_baseline_full_path = get_full_path('overview_baseline.csv')
+    df_overview_baseline = pd.read_csv(overview_baseline_full_path)
+
     fig = make_subplots(specs=[[{"secondary_y": True}]])
 
+    # Fps
     fig.add_trace(
         go.Scatter(
-            x=df_fps['day'], y=df_fps['average'], name='average fps', line=dict(color='royalblue', width=4)
+            x=df_fps['day'],
+            y=df_fps['average'],
+            name='average fps',
+            line=dict(color='royalblue', width=4)
         ),
         secondary_y=False
     )
 
     fig.add_trace(
         go.Scatter(
-            x=df_overview['day'], y=df_overview['amount'], name='# animals', line=dict(color='darkgrey', width=4)
+            x=df_fps_baseline['day'],
+            y=df_fps_baseline['average'],
+            name='average fps baseline',
+            line=dict(color='firebrick', width=4)
+        ),
+        secondary_y=False
+    )
+
+    # Amount animals
+    fig.add_trace(
+        go.Scatter(
+            x=df_overview['day'],
+            y=df_overview['amount'],
+            name='# animals',
+            line=dict(color='navy', width=4),
+            opacity=0.5
+        ),
+        secondary_y=True
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=df_overview_baseline['day'],
+            y=df_overview_baseline['amount'],
+            name='# animals baseline',
+            line=dict(color='lightcoral', width=4),
+            opacity=0.5
         ),
         secondary_y=True
     )
