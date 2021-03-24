6b4225dd9a4844ff9b15bec0ef639b6f19540139
Theo <theo.le.wiik@gmail.com>
2021-03-24 18:16:57 +0100

    Calculate average values by species

--- Plotting/average.py
+++ Plotting/average.py
@@ -1,20 +1,64 @@
-import pandas as pd
-import plotly.express as px
+import json
+
+import plotly.graph_objects as go
 
 from util.file_finder import get_full_path
+from util.json_extracter import filter_where, extract_unique
+
+
+def create_scatter(data, species, color):
+    days = extract_unique('day', data)
+    sizes = []
+    speeds = []
+
+    for day in days:
+        relevant_data = filter_where(
+            filter_where(data, 'species', species),
+            'day',
+            day
+        )
+
+        if len(relevant_data) == 0:
+            continue
+
+        size = sum(i['size'] for i in relevant_data) / len(relevant_data)
+        sizes.append(size)
+
+        speed = sum(i['speed'] for i in relevant_data) / len(relevant_data)
+        speeds.append(speed)
+
+    return go.Scatter3d(
+        x=days,
+        y=sizes,
+        z=speeds,
+        marker=dict(
+            color=color,
+        )
+    )
 
 
 def plot():
-    full_path = get_full_path('overview.csv')
+    full_path = get_full_path('detailed.json')
+    f = open(full_path)
+    data = json.load(f)
 
-    df = pd.read_csv(full_path)
-    fig = px.scatter_3d(
-        df,
-        x='day',
-        y='speed',
-        z='size',
-        color='amount',
-        title='Average Values'
+    fig = go.Figure()
+
+    fig.add_trace(
+        create_scatter(data, 'Rabbit', 'royalblue')
+    )
+
+    fig.add_trace(
+        create_scatter(data, 'Wolf', 'red')
+    )
+
+    fig.update_layout(
+        title_text='Average Values by Species',
+        scene=dict(
+            xaxis_title='day',
+            yaxis_title='size',
+            zaxis_title='speed'
+        )
     )
 
     fig.show()
--- Plotting/util/json_extracter.py
+++ Plotting/util/json_extracter.py
@@ -1,2 +1,22 @@
 def extract(key, data):
     return [i[key] for i in data]
+
+
+def filter_where(data, key, value_equals):
+    output = []
+
+    for row in data:
+        if row[key] == value_equals:
+            output.append(row)
+
+    return output
+
+
+def extract_unique(key, data):
+    output = []
+
+    for item in extract(key, data):
+        if not output.__contains__(item):
+            output.append(item)
+
+    return output
