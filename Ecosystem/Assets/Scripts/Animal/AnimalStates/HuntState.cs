62d6b08106e377a11ec0a08f2aa2d55e2259f1db
Emil Wingårdh <wiemil@student.chalmers.se>
2021-03-31 13:59:32 +0200

    Fix so wolf doesn't stop hunting even if stamina is 0 when target is dead or in reach

--- Ecosystem/Assets/Scripts/Animal/AnimalStates/HuntState.cs
+++ Ecosystem/Assets/Scripts/Animal/AnimalStates/HuntState.cs
@@ -29,13 +29,17 @@ namespace Animal.AnimalStates
 
     public AnimalState Execute()
     {
-       _target = _carnivore.Target;
+      _target = _carnivore.Target;
+      var position = _carnivore.transform.position;
+      var closestPoint = _target.animalCollider.ClosestPointOnBounds(position);
+      _carnivore.GoTo(closestPoint);
+      
       if (!_carnivore.Alive) return AnimalState.Dead;
       if (_carnivore.IsThirsty && !_carnivore.KnowsWaterLocation && !_carnivore.IsHungry)
         return AnimalState.SearchWorld;
       if (!_carnivore.ShouldHunt(_target))
         return AnimalState.Wander;
-      if (_carnivore.GetStaminaDelegate().StaminaZero)
+      if (_carnivore.GetStaminaDelegate().StaminaZero && !_target.Dead && Vector3.Distance(position, closestPoint) > _carnivore.Reach + 1f)
       {
         _carnivore.Target = null;
         _carnivore.GetStaminaDelegate().IncreaseStamina(3);
@@ -47,10 +51,7 @@ namespace Animal.AnimalStates
         _carnivore.Target = null;
         return AnimalState.Wander;
       }
-
-      var position = _carnivore.transform.position;
-      var closestPoint = _target.animalCollider.ClosestPointOnBounds(position);
-      _carnivore.GoTo(closestPoint);
+      
       
       if (Vector3.Distance(position, closestPoint) < _carnivore.Reach)
       {
