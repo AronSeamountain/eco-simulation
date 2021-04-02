2c740477bcdc56aaaae408a8d9a3b946c02f31e1
Viktor Fredholm <vikfre@student.chalmers.se>
2021-04-02 15:21:03 +0200

    attempt at logging

---
+++ Plotting/sense_averages.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+from util.file_finder import get_full_path
+
+def plot():
+    full_path = get_full_path('overview.csv')
+    df = pd.read_csv(full_path)
+    fig = go.figure()
+
+    fig.add_trace(
+        go.scatter(
+            x = df['day'], 
+            y = df['hearingPercentage'], 
+            name = 'hearing percentage', 
+            line = dict(color = 'firebrick', width = 4)
+        )
+    )
+
+    
+    fig.add_trace(
+        go.scatter(
+            x = df['day'], 
+            y = df['visionPercentage'], 
+            name = 'vision percentage', 
+            line = dict(color = 'royalblue', width = 4)
+        )
+    )
+
+    fig.update_layout(
+        title = 'Distribution of vision and hearing power',
+        xaxis_title='day',
+        yaxis_title='percentage'
+    )
+
+    fig.show()
+
+if __name__ == '__main__':
+    plot()
