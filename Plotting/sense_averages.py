e01a3e94e735c4623a677e365ca2fa2bd584b904
Viktor Fredholm <vikfre@student.chalmers.se>
2021-04-08 14:11:31 +0200

    multiple graphs

--- Plotting/sense_averages.py
+++ Plotting/sense_averages.py
@@ -14,24 +14,25 @@ from util.json_extracter import extract_unique
 
 def plot():
     full_path = get_full_path('detailed.json')
-    fig = go.Figure()
+    fig = make_subplots(rows = 2, cols = 1, subplot_titles=("Rabbit senses", "Wolf senses"))#go.figure()
     data = json.load(open(full_path))
 
     all_days = extract_unique('day', data)
 
 #TODO make all of this into a metho dewhen something works
-    fig.add_trace(
+    fig.append_trace(
         go.Scatter(
             x = all_days, 
             y = calcAverage(data, 'Rabbit', 'hearingPercentage'), 
             line = dict(color = 'firebrick', width = 4),
-            name = 'Vision',
+            name = 'Eyesight',
             stackgroup='one',
             groupnorm='percent'
-        )
+        ),
+        row=1, col=1
     )
 
-    fig.add_trace(
+    fig.append_trace(
         go.Scatter(
             x = all_days, 
             y = calcAverage(data, 'Rabbit', 'visionPercentage'), 
@@ -39,21 +40,72 @@ def plot():
             name = 'Hearing',
             stackgroup='one',
             groupnorm='percent'
-        )
+        ),
+        row=1, col=1
+    )
+
+    #wolf graph
+    fig.append_trace(
+        go.Scatter(
+            x = all_days, 
+            y = calcAverage(data, 'Wolf', 'hearingPercentage'), 
+            line = dict(color = 'firebrick', width = 4),
+            name = 'Eyesight',
+            stackgroup='one',
+            groupnorm='percent'
+        ),
+        row=2, col=1
+    )
+
+    fig.append_trace(
+        go.Scatter(
+            x = all_days, 
+            y = calcAverage(data, 'Wolf', 'visionPercentage'), 
+            line = dict(color = 'royalblue', width = 4),
+            name = 'Hearing',
+            stackgroup='one',
+            groupnorm='percent'
+        ),
+        row=2, col=1
     )
 
     fig.update_layout(
         showlegend=True,
-        title = 'Distribution of vision and hearing power',
-        xaxis_title='day',
-        yaxis_title='percentage',
-        xaxis_type='category',
-        yaxis=dict(
-            type='linear',
-            range=[1, 100],
-            ticksuffix='%'))
+        title = 'Distribution of eyesight and hearing power',
+    )
     
+    fig.update_yaxes(
+        title='Percentage',  
+        type='linear',
+        range=[1, 100],
+        ticksuffix='%',
+        row = 1,
+        col = 1
+    )
     
+    fig.update_xaxes(
+        title='Day',
+        type = 'category',
+        row = 1,
+        col = 1
+    )
+
+    fig.update_yaxes(
+        title='Percentage',  
+        type='linear',
+        range=[1, 100],
+        ticksuffix='%',
+        row = 2,
+        col = 1 
+    )
+
+    fig.update_xaxes(
+        title='Day',
+        type = 'category',
+        row = 2,
+        col = 1
+    )
+
     fig.show()
 
 def calcAverage(data, species, dataToConsider):
@@ -65,8 +117,8 @@ def calcAverage(data, species, dataToConsider):
     for day in all_days:
         values = [i[dataToConsider] for i in data if i['day'] == day and i['species'] == species]
 
-     
-        average = sum(values)/len(values)
+        average = sum(values)
+        if len(values) != 0: average = sum(values)/len(values)
 
         averages.append(average)
     
