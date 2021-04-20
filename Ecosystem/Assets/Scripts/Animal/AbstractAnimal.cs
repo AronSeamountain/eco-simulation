f98c6697211ae8419afa2694b4260e94f3dfea91
Yvonne Hansson <gushansyv@student.gu.se>
2021-04-20 13:54:47 +0200

    Wolves died out at approx day 540

--- Ecosystem/Assets/Scripts/Animal/AbstractAnimal.cs
+++ Ecosystem/Assets/Scripts/Animal/AbstractAnimal.cs
@@ -259,7 +259,7 @@ namespace Animal
       }
       if (OptionsMenu.World == "Medium-scene")
       {
-        if (IsCarnivore) _timeUntilRememberWater += 70;
+        if (IsCarnivore) _timeUntilRememberWater += 20;
         else _timeUntilRememberWater += 1;
       }
       KnowsWaterLocation = false;
