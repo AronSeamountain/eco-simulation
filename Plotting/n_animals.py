8e4c9b221a3feb9947a4b96536d7b0a2f37a526d
Theo <theo.le.wiik@gmail.com>
2021-04-28 14:19:02 +0200

    Plot all overview.csv files

--- Plotting/n_animals.py
+++ Plotting/n_animals.py
@@ -1,11 +1,29 @@
+import sys
+
 import pandas as pd
 import plotly.graph_objects as go
 
-from util.file_finder import get_full_path
+from util.file_finder import get_full_paths
 
 
 def plot():
-    full_path = get_full_path('overview.csv')
+    full_paths = get_full_paths('overview.csv')
+    plot_all = False
+
+    if len(sys.argv) >= 2 and str(sys.argv[1]) == 'all':
+        plot_all = True
+
+    if plot_all:
+        print('Plotting all overview.csv files')
+
+        for full_path in full_paths:
+            plot_this(full_path)
+    else:
+        print('Plotting first overview.csv found')
+        plot_this(full_paths[0])
+
+
+def plot_this(full_path):
     df = pd.read_csv(full_path)
     fig = go.Figure()
 
@@ -22,7 +40,7 @@ def plot():
     )
 
     fig.update_layout(
-        title='Amount of Animals',
+        title='Amount of Animals (' + full_path + ')',
         xaxis_title='day',
         yaxis_title='amount'
     )
